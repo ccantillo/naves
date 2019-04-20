046f67703192f556339a829588525aa7b7346722
ccantillo <43839760+ccantillo@users.noreply.github.com>
2019-04-20 01:19:36 -0500

    Update preload.js

--- JS/preload.js
+++ JS/preload.js
@@ -9,7 +9,7 @@ class Cargar extends Phaser.Scene {
       this.scene.start("sceneA");
     })
     this.load.image('player', 'assets/playerShip1_blue.png');
-    this.load.image('base', 'assets/ufoRed.PNG');
+    this.load.image('base', 'assets/ufoRed.png');
     this.load.image('background', 'assets/purple.png');
     this.load.image('bala', 'assets/laserBlue03.png');
     this.load.image('enemigo','assets/enemyRed32.png')
